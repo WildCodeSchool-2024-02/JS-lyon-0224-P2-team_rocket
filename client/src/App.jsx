e978bd53e18ade625cea125e66d5da90092eb505
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-04-09 23:38:57 +0200

    App.jsx ajout useEffect + PokedexCard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import axios from "axios";
-import { useState } from "react";
-import PokeCard from "./components/PokeCard";
+import { useEffect, useState } from "react";
+// import PokeCard from "./components/PokeCard";
+import PokedexCard from "./components/PokedexCard/PokedexCard";
 
 function App() {
-  const [pokemons, setPokemons] = useState();
+  const [pokemons, setPokemons] = useState([]);
+
   const getPokemon = () => {
     axios
       .get("https://pokebuildapi.fr/api/v1/pokemon/limit/100")
@@ -12,13 +14,17 @@ function App() {
         setPokemons(response.data);
       });
   };
+  useEffect(() => {
+    getPokemon();
+  }, []);
 
   return (
     <main className="container">
-      <PokeCard getPokemon={getPokemon} pokemons={setPokemons} />
-
-      {/* Travail à effacer */}
-      {pokemons}
+      {pokemons.length > 0 ? (
+        <PokedexCard pokemons={pokemons} setPokemons={setPokemons} />
+      ) : (
+        <p>Lodading Pokemon</p>
+      )}
     </main>
   );
 }
