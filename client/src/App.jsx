13dee0ee5b528ac367bf9de7111f669d1afb9106
Joris Budin <jorisbudin@gmail.com>
2024-04-18 11:09:30 +0200

    fix style pokecardMobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,14 @@ function App() {
 
   const [pokemons, setPokemons] = useState(pokemonsData);
   const [random, setRandom] = useState(0);
+  function getNewPoke() {
+    setRandom(parseInt(Math.random() * 100, 10));
+  }
 
   return (
     <main className="container">
       <Outlet context={{ pokemons, setPokemons, random, setRandom }} />
-      <Navbar />
+      <Navbar getNewPoke={getNewPoke()} />
     </main>
   );
 }
