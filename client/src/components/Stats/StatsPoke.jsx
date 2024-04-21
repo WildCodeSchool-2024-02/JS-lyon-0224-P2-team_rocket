6b67619aaeb8d6c37b9e60255630d15d0a92e326
joris budin <jorisbudin@gmail.com>
2024-04-21 19:41:49 +0200

    Add StatsPoke.module.css

--- client/src/components/Stats/StatsPoke.jsx
+++ client/src/components/Stats/StatsPoke.jsx
@@ -1,21 +1,22 @@
 import PropTypes from "prop-types";
+import styles from "./StatsPoke.module.css";
 
 function StatsPoke({ pokemons, random, typeImgUrl }) {
   return (
-    <div id="stats" className={typeImgUrl[0].color}>
-      <span className="stat">
+    <div id={styles.stats} className={typeImgUrl[0].color}>
+      <span className={styles.stat}>
         <h3>HP</h3>
         <p>{pokemons[random].stats.HP}</p>
       </span>
-      <span className="stat">
+      <span className={styles.stat}>
         <h3>Attaque</h3>
         <p>{pokemons[random].stats.attack}</p>
       </span>
-      <span className="stat">
+      <span className={styles.stat}>
         <h3>Defense</h3>
         <p>{pokemons[random].stats.defense}</p>
       </span>
-      <span className="stat">
+      <span className={styles.stat}>
         <h3>Speed</h3>
         <p>{pokemons[random].stats.speed} </p>
       </span>
