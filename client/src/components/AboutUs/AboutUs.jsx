25884e43edea7d8ae7163725ff363a9842804797
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-04-22 16:17:36 +0200

    style img About us team rocket

--- client/src/components/AboutUs/AboutUs.jsx
+++ client/src/components/AboutUs/AboutUs.jsx
@@ -1,13 +1,24 @@
 import styles from "./AboutUs.module.css";
 
+import james from "../../assets/images/avatar/james.png";
+import jessie from "../../assets/images/avatar/jessie.png";
+import miaouss from "../../assets/images/avatar/miaouss.png";
+
 function AboutUs() {
   return (
-    <>
-      <div className={styles.container_about}>AboutUs</div>
-      <div>Joris</div>
-      <div>Flakron</div>
-      <div>Anne-so</div>
-    </>
+    <div className={styles.container_about}>
+      <div className={styles.jessie}>
+        <img src={jessie} alt="jessie-team" style={{ width: "100%" }} />
+      </div>
+
+      <div className={styles.james}>
+        <img src={james} alt="james-team" style={{ width: "100%" }} />
+      </div>
+
+      <div className={styles.miaouss}>
+        <img src={miaouss} alt="miaouss-team" style={{ width: "100%" }} />
+      </div>
+    </div>
   );
 }
 
